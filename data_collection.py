3ebfc82b0175e5601f25a187d2bc52bd421a7e18
Daniel Handoyo <daniel023@e.ntu.edu.sg>
2024-02-02 00:38:12 +0800

    Updating first parliament sitting dates

--- data_collection.py
+++ data_collection.py
@@ -5,15 +5,21 @@ import requests
 from bs4 import BeautifulSoup
 import regex as re
 
+parl1 = ['08-12-1965', '13-12-1965', '16-12-1965', '17-12-1965', '20-12-1965', '21-12-1965', '22-12-1965', '23-12-1965',
+         '24-12-1965', '28-12-1965', '29-12-1965', '30-12-1965', '31-12-1965', '23-2-1966', '21-4-1966', '22-6-1966',
+         '17-8-1966', '26-8-1966', '26-10-1966', '5-12-1966', '12-12-1966', '14-12-1966', '15-12-1966', '16-12-1966',
+         '19-12-1966', '20-12-1966', '21-12-1966', '27-2-1967', '13-3-1967', '14-3-1967', '15-3-1967', '16-3-1967',
+         '17-3-1967', '24-5-1967', '29-6-1967', '7-9-1967', '8-9-1967', '31-10-1967', '2-11-1967', '14-11-1967',
+         '30-11-1967', '5-12-1967', '11-12-1967', '14-12-1967', '15-12-1967', '18-12-1967', '19-12-1967', '20-12-1967',
+         '21-12-1967', '24-1-1968']
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Ensures the browser window won't be shown
-
-driver = webdriver.Chrome(options = chrome_options)
-driver.get('https://sprs.parl.gov.sg/search/#/topic?reportid=022_19680124_S0003_T0008')
-
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(f'https://sprs.parl.gov.sg/search/#/report?sittingdate={parl1[0]}')
 webpage = driver.page_source
-soup = BeautifulSoup(webpage, 'lxml')
+soup = BeautifulSoup(webpage, 'html.parser')
+
 meta_tags = soup.find_all('meta')
 meta_data = {}
 
@@ -26,52 +32,7 @@ for tag in meta_tags:
 
 print(meta_data)
 
+exit()
+
 table = soup.find('div', class_='reportTable')
-print(table)
-exit()
-
-
-"""
-print(soup.find_all('h1')[0].text)  # h[n], n = size of text
-
-print(len(soup.find_all('h1')))  # Number of h1 tags
-
-for i in soup.find_all('h2'):
-    print(i.text.strip())
-
-print("P tags:")
-for i in soup.find_all('p'):
-    print(i.text.strip())
-"""
-
-# Analysing each company profile on the webpage
-report = soup.findall('div', class_='reportTable')
-print(report)  # Visualise the company sections
-exit()
-
-
-
-
-print(len(company))  # Prints number of companies on the page
-name = []
-rating = []
-reviews = []
-ownership = []
-for i in company:
-    name.append(i.find('h2').text.strip())
-    rating.append(i.find('span', class_='companyCardWrapper__companyRatingValue').text.strip())
-    reviews.append(i.find('span', class_='companyCardWrapper__ActionCount').text.strip())
-
-    # Extract ownership information
-    ownership_text = i.find('span', class_='companyCardWrapper__interLinking').text.strip()
-    ownership_match = re.search(r'\b(Public|Private)\b', ownership_text)
-    if ownership_match:
-        ownership.append(ownership_match.group(0))
-    else:
-        ownership.append('Private')
-
-data = {'Name': name, 'Rating': rating, 'Reviews': reviews, 'Ownership': ownership}
-df = pd.DataFrame(data)
-print(df)
-
-
+print(table.text.strip())
